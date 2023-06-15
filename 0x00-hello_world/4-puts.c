46705b910ef42c7ab488efa76855a876c5f7fee8
volemy <minayobenevolence0'gmail.com>
2023-06-15 11:34:48 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main -"A program that uses the puts function to print a line
+ *  Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
