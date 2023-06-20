5630ccf6e4cd44fb90a498f783fc6033e921a71a
volemy <minayobenevolence0'gmail.com>
2023-06-20 16:31:45 -0700

    Write a function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,8 +6,8 @@
  *
  * Description: check if it prints sign of a number
  * Return: 1 and prints + if n is greater than zero
- * Return: 0 and prints 0 if n is zero
- * Return: -1 and prints - if n is less than zero
+ *         prints 0 if n is zero
+ *          prints - if n is less than zero
  */
 int print_sign(int n)
 {
@@ -18,7 +18,7 @@ return (1);
 
 }
 else if (n == 0)
-{	
+{
 _putchar(48);
 return (0);
 }
