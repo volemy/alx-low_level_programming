0f95e3e2971f525c4c36be7e72409801fbf1c35a
volemy <minayobenevolence0'gmail.com>
2023-06-20 04:28:44 -0700

    Write a program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-        putchar('95');
-        putchar('112');
-        putchar('117');
-        putchar('116');
-        putchar('99');
-        putchar('104');
-        putchar('97');
-        putchar('114');
-        putchar('10');
+        putchar(95);
+        putchar(112);
+        putchar(117);
+        putchar(116);
+        putchar(99);
+        putchar(104);
+        putchar(97);
+        putchar(114);
+        putchar(10);
 
         return (0);
 }
