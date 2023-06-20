d84cf8e6dd6b1677a3e89babe2503b82c55ac1d1
volemy <minayobenevolence0'gmail.com>
2023-06-20 04:02:35 -0700

    Write a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - A program thats prints _putchar
+ *
+ * Description: check if it prints _putchar
+ * Return: 0 (success)
+ */
+int main(void)
+{
+        putchar('95');
+        putchar('112');
+        putchar('117');
+        putchar('116');
+        putchar('99');
+        putchar('104');
+        putchar('97');
+        putchar('114');
+        putchar('10');
+
+        return (0);
+}
