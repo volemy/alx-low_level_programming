b35c2177dbe795095cf05f69b8925c5b500a833a
volemy <minayobenevolence0'gmail.com>
2023-06-20 07:48:41 -0700

    Header files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,18 @@
  * Description: check if it prints all alphabets in lowercase
  *
  * print_alphabet - print all alphabet in lowercase
+ * Return: 0 (success)
  */
 void print_alphabet(void)
 {
-	int char ch;
+	char ch;
 
 	for (ch = 97; ch <= 122; ch++)
 		_putchar(ch);
 
 	_putchar(10);
+
+	return (0);
 
 }
 
