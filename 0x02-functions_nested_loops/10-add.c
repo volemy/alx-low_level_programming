f8b3332c5071edb6f5d4e50b67ed148baee12e32
volemy <minayobenevolence0'gmail.com>
2023-06-21 06:43:29 -0700

    Write a function that adds two integers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -10,15 +10,13 @@
  *
  * Return: sum of a and b
  */
-int add(int, int)
-	int a;
-	int b;
+int add(int a, int b)
 
 {
 
 int sum; /* Declaration of the variable sum */
 
-sum = int a + int b;
+sum = a +  b;
 
 return (sum);
 
