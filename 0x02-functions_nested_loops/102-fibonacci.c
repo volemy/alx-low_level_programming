c9059e936b920976170c79dd1dfcc7474da9334c
volemy <minayobenevolence0'gmail.com>
2023-06-21 15:46:00 -0700

    Write a program that prints the first 50 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,12 +14,12 @@ int main(void)
 int count;
 unsigned long fibo1 = 0, fibo2 = 1, sum;
 
-for (count = 0; count < 50; count ++)
+for (count = 0; count < 50; count++)
 
 {
 
-sum = fibo1 = fibo2;
-printf("%lu", sum);
+sum = fibo1 + fibo2;
+printf("%lu", fibo1);
 
 fibo1 = fibo2;
 fibo2 = sum;
