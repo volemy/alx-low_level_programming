791112fb70540022d0a530aa861952175a7224c5
volemy <minayobenevolence0'gmail.com>
2023-06-20 17:53:08 -0700

    Write a function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ for (k = 0; k < 10; k++)
 
 {
 
-l = k; 42 j;
+l = k * j;
 if (k == 0)
 {
 _putchar(l + '0');
@@ -37,6 +37,6 @@ _putchar((l / 10) + '0');
 _putchar((l % 10) + '0');
 }
 }
--putchar('\n');
+_putchar('\n');
 }
 }
