786c121af08bde1b9d5052b1901b9bd8ec8b8110
volemy <minayobenevolence0'gmail.com>
2023-06-22 16:11:55 -0700

    Write a function that prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,19 +19,20 @@ void print_triangle(int size)
 	else
 	{
 		int a, b;
-		
+
 		for (a = 1; a <= size; a++)
 		{
 			for (b = 1; b <= size; b++)
 			{
 				_putchar(' ');
 			}
-			
-			for (b = 1; b <= a; b++);
+
+			for (b = a; b <= a; b++)
 			{
 				_putchar('#');
 			}
-			
+
+
 			_putchar('\n');
 
 		}
