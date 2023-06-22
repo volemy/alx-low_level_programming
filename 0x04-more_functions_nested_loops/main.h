7fff881f49153ba5ac07845a3bf217fdcd029089
volemy <minayobenevolence0'gmail.com>
2023-06-22 05:18:34 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,29 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-int _isupper(int c)
-int _isdigit(int c)
-int mul(int a, int b)
-void print_numbers(void)
-void print_most_numbers(void)
-void more_numbers(void)
-void print_line(int n)
-void print_diagonal(int n)
-void print_square(int size)
-void print_triangle(int size)
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
 #endif /* MAIN_H */
