d00121c93c3ec5769a29406d5f91d46d2ef501a0
volemy <minayobenevolence0'gmail.com>
2023-06-26 07:44:36 -0700

    Header

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
-ifendf
-
-
+#ifndef MAIN_H
+#define MAIN_H 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -11,3 +10,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
+#endif /* MAIN_H*/
