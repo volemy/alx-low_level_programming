fe485ceb23b8fc630b9d27a4a7c2eb08cec28953
volemy <minayobenevolence0'gmail.com>
2023-06-26 08:31:22 -0700

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - function that takes a pointer to an integer and updates its value
- * @n:integer input pointer
+ * reset_to_98 - function that takes a pointer to an integer and updates its value
+ * @n: integer input pointer
  *
  * Desription: check if it updates the value it points to to 98
  *
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - function that swaps the values of two integers
+ * @a: first integer to swap
+ * @b: second integer to swap
+ *
+ * Description: check if function swaps the integers values
+ * Return: 0 (success)
+ */
+void swap_int(int *a, int *b)
+
