dccbd352f88b49a0dafe7f389230cdf1886659a7
volemy <minayobenevolence0'gmail.com>
2023-07-27 17:25:55 -0700

    a function that prints a sentence

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * initial -function that  applies the constructor attribute to my initial() so it's 
+ * executed before the main()
+ *
+ */
+void initial(void)__attribute__((constructor));
+
+/**
+ * initial - implementation of  initial
+ */
+void initial(void)
+{
+	printf("you're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
