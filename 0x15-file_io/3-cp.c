8d7a0677f9d569854fe7c3c3d20e56e08bb7910e
volemy <minayobenevolence0@gmail.com>
2023-08-07 23:18:28 +0000

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -37,7 +37,7 @@ void close_file(int fd)
 
 	if (k == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n"), fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
