eb862762cbc1627cb5dcda5bd1c7a87bcf9677d2
volemy <minayobenevolence0@gmail.com>
2023-08-07 19:09:05 +0000

    Header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,10 @@
 #ifndef FILE_I_O_H
 #define FILE_I_O_H
 
-#include <stdlib.h>
+#include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
