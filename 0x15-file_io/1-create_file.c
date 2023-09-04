6d39f8e67c71a3cc6e560aeac4f552052c8d8734
volemy <minayobenevolence0@gmail.com>
2023-09-04 21:41:57 +0000

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,33 +13,36 @@
 
 int create_file(const char *filename, char *text_content)
 {
-        int fd, strlen, bytes;
+	int fd, strlen, bytes;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        if (text_content == NULL)
-                text_content = "";
+	if (text_content == NULL)
+		text_content = "";
 
-        fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-                if (fd == -1)
-                return (-1);
+	if (fd == -1)
+		return (-1);
 
-        if (text_content != NULL)
-        {
-                for (strlen = 0; text_content[strlen];)
-                        strlen++;
-        }
+	if (text_content != NULL)
+	{
+		for (strlen = 0; text_content[strlen];)
+			strlen++;
 
-        bytes = write(fd, text_content, strlen);
-        if (bytes == -1)
-        {
-                close(fd);
-                return (-1);
-        }
+	}
 
-        close(fd);
+	bytes = write(fd, text_content, strlen)
 
-        return (1);
+		if (bytes == -1)
+		{
+			close(fd);
+			return (-1);
+
+		}
+
+	close(fd);
+
+	return (1);
 }
