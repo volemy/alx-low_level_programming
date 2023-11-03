81a2440f9e100e0ec229f7702d636f5e53211608
volemy <minayobenevolence0@gmail.com>
2023-11-03 16:46:23 +0000

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,7 +3,8 @@
 /**
  * hash_table_create - Creates a hash table.
  * @size: The size of the array.
- * Return: A pointer to the newly created hash table,or NULL otherwise.
+ *
+ * Return: A pointer to the newly created hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
