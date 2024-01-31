3851dcf2d7d9f1ec5b16dedd547acddd80a0e5cc
volemy <minayobenevolence0@gmail.com>
2024-01-31 01:56:37 +0000

    task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,48 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to a hash table
+ * @ht: the hash table to add the key to
+ * @key: the key to add to the table
+ * @value: the value to add to the table
+ *
+ * Return: 1 if it succeeded, 0 if it failed
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    unsigned long int index;
+    hash_node_t *new_node, *tmp;
+
+    if (ht == NULL || key == NULL || value == NULL)
+        return (0);
+
+    index = key_index((const unsigned char *)key, ht->size);
+
+    tmp = ht->array[index];
+
+    while (tmp != NULL)
+    {
+        if (strcmp(tmp->key, key) == 0)
+        {
+            free(tmp->value);
+            tmp->value = strdup(value);
+            if (tmp->value == NULL)
+                return (0);
+            return (1);
+        }
+        tmp = tmp->next;
+    }
+
+    new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+        return (0);
+
+    new_node->key = strdup(key);
+    new_node->value = strdup(value);
+    new_node->next = ht->array[index];
+
+    ht->array[index] = new_node;
+
+    return (1);
+}
